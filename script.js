4eca5e6edd62035f53be1b02f0ba5c9bc10e27be
FrozenWasabi <albexlong@gmail.com>
2022-08-27 15:13:56 -0400

    added the function to create a summary

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function createArticleID() {
-    let linkInput = document.getElementsByClassName("txtinput")
+    var articleText = "I am an 84-year-old gentleman who stands 5’4” tall from the basketball-crazy state of Indiana. Recently, my wife and I were having dinner at a local restaurant. Our waiter was a young man, around 6’8”. Naturally, I asked him if he played basketball. He looked down at me, replied, “Yes, I do,” and then asked me if I played miniature golf."
     var settings = {
         "url": "https://api.symbl.ai/v1/process/text",
         "method": "POST",
@@ -14,7 +14,7 @@ function createArticleID() {
           "messages": [
             {
               "payload": {
-                "content": "this is a stringfile",
+                "content": articleText,
                 "contentType": "text/plain"
               },
               "from": {
@@ -32,18 +32,22 @@ function createArticleID() {
 }
 
 function createSummary() {
+    var urlID = "https://api-labs.symbl.ai/v1/conversations/"
+    ID = "6317660938174464"
+    urlID += ID
+    urlID += "/summary?eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjQ4Nzk1MTA0ODMyMzg5MTIiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiY0hXT1ZsOXNJSlp0d29mREZuVlY5SlBYb3VhMG1ObHZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjYxNjA5ODE1LCJleHAiOjE2NjE2OTYyMTUsImF6cCI6ImNIV09WbDlzSUpadHdvZkRGblZWOUpQWG91YTBtTmx2IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.XGtaIRNTHPNV_t_aaZJVia7vND423Y-8EQOPt3Rc_Q7DmL29j_k9kPYf-OVHRXmRwuyD8OFXcA_BavutAuJv1IFE8iwJER4myLM9ZElEcYRUiWO1t5NZNUrIVSa4lx7Jh0bK5Gfga-trumPBd1MTZGpyP27IHGs2xUNubLRALTAdx-WmdM4kIHrZpAe8fUwCWIbXSvoHztCuz-q9BEnhPvIl91Tl7jsmdELRzQaT65AWI-Dppth_z5_Ljeptj_suUZDPeymzGqlpK-P364qyPXhBopgKI_9M5PqoNRaZ6jDqNg4g0Dmi80lgh2t744uEJ_S0l6sKUiGI9q4pzPf3ww"
     var settings = {
-        "url": "https://api-labs.symbl.ai/v1/conversations/(ID)/summary?eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjQ4Nzk1MTA0ODMyMzg5MTIiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiY0hXT1ZsOXNJSlp0d29mREZuVlY5SlBYb3VhMG1ObHZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjYxNjA5ODE1LCJleHAiOjE2NjE2OTYyMTUsImF6cCI6ImNIV09WbDlzSUpadHdvZkRGblZWOUpQWG91YTBtTmx2IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.XGtaIRNTHPNV_t_aaZJVia7vND423Y-8EQOPt3Rc_Q7DmL29j_k9kPYf-OVHRXmRwuyD8OFXcA_BavutAuJv1IFE8iwJER4myLM9ZElEcYRUiWO1t5NZNUrIVSa4lx7Jh0bK5Gfga-trumPBd1MTZGpyP27IHGs2xUNubLRALTAdx-WmdM4kIHrZpAe8fUwCWIbXSvoHztCuz-q9BEnhPvIl91Tl7jsmdELRzQaT65AWI-Dppth_z5_Ljeptj_suUZDPeymzGqlpK-P364qyPXhBopgKI_9M5PqoNRaZ6jDqNg4g0Dmi80lgh2t744uEJ_S0l6sKUiGI9q4pzPf3ww",
-        "method": "GET",
-        "timeout": 0,
-        "headers": {
-          "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjQ4Nzk1MTA0ODMyMzg5MTIiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiY0hXT1ZsOXNJSlp0d29mREZuVlY5SlBYb3VhMG1ObHZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjYxNjE1NDUxLCJleHAiOjE2NjE3MDE4NTEsImF6cCI6ImNIV09WbDlzSUpadHdvZkRGblZWOUpQWG91YTBtTmx2IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.MsprMhEh5-Sv3pGv6meuQB-M2CXfkcaD8G9RRTAUkLigmpRZcZOHlv39_74eu4d6o8cjadflfffn-40vO9f4edz_7WAqVVixjVwfSkC9OiJXanDBavDbTOkKKoO0nHir5E0LG8cqM6Mhmh3SQIvADZMp-ftY0MMO2Na5l45LiNY6nEE9U3RZG7K8Y9V0O2danBk2wz2D5HpqNY2ZA7fH83DxFdmIvmQVyUd6YPOA8iElUC5feu30DyFNebANwi2WrZ6K509ITFd6zhqflYgBjnomx0ZdtRfsYnAwdwJelQ0yDguiZu4mnclXw8b6LEnfofs0WQ1JZ1GcZFRyOqWwGA"
-        },
-      };
-      
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-      });
+      "url": urlID,
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjQ4Nzk1MTA0ODMyMzg5MTIiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiY0hXT1ZsOXNJSlp0d29mREZuVlY5SlBYb3VhMG1ObHZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjYxNjE1NDUxLCJleHAiOjE2NjE3MDE4NTEsImF6cCI6ImNIV09WbDlzSUpadHdvZkRGblZWOUpQWG91YTBtTmx2IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.MsprMhEh5-Sv3pGv6meuQB-M2CXfkcaD8G9RRTAUkLigmpRZcZOHlv39_74eu4d6o8cjadflfffn-40vO9f4edz_7WAqVVixjVwfSkC9OiJXanDBavDbTOkKKoO0nHir5E0LG8cqM6Mhmh3SQIvADZMp-ftY0MMO2Na5l45LiNY6nEE9U3RZG7K8Y9V0O2danBk2wz2D5HpqNY2ZA7fH83DxFdmIvmQVyUd6YPOA8iElUC5feu30DyFNebANwi2WrZ6K509ITFd6zhqflYgBjnomx0ZdtRfsYnAwdwJelQ0yDguiZu4mnclXw8b6LEnfofs0WQ1JZ1GcZFRyOqWwGA"
+      },
+    };
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
 }
 
 function printEndResult() {
